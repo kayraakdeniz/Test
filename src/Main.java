306054a9c5ada7740c7e18c209c61393d86081ce
Hp <kayraakdeniz@yahoo.com>
2022-05-02 15:14:14 +0300

    Main class changed 2nd time

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("merhaba");
+        System.out.println("Main class changed for 2nd time");
     }
 }
